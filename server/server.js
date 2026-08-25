2a23849a062859c4dc3cc13003694196629016ad
Nitin Rao <nitincreative00@gmail.com>
2026-08-26 03:59:34 +0530

    Fix Vercel routing and server config

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
 
+
 dotenv.config();
 
 const app = express();
@@ -22,6 +23,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
